fdf0d9877ca303c73ac03392b829c30f6307982b
FranckNdame <franck.mpouli@yahoo.com>
2019-12-06 11:10:56 +0000

    add project files

---
+++ backprop-neural-net/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  backprop-neural-net
+//
+//  Created by Franck-Stephane Ndame Mpouli on 06/12/2019.
+//  Copyright © 2019 Franck-Stephane Ndame Mpouli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
