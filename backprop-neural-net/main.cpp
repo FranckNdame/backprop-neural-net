6bdd98d70adea686a472e45e5085cfda32306399
FranckNdame <franck.mpouli@yahoo.com>
2019-12-06 11:35:05 +0000

    create Network class

--- backprop-neural-net/main.cpp
+++ backprop-neural-net/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <vector>
+
+class Network
+{
+public:
+    // Constructor
+    Network(const std::vector<unsigned> &architecture);
+    
+    //
+    void feedFoward(const std::vector<double> &inputs);
+    void backPropagate(const std::vector<double> &targets);
+    // const qualifiers
+    void getResults(std::vector<double> &results) const;
+private:
+    
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::vector<unsigned> architecture;
+    
+    Network network(architecture);
+    std::vector<double> inputs;
+    network.feedFoward(inputs);
+    
+    std::vector<double> targets;
+    network.backPropagate(targets);
+    
+    std::vector<double> results;
+    network.getResults(results);
+    
+    
     return 0;
 }
